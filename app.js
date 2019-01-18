f3c7f4b64c6723bec81f71cf850f93c63e9510af
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-18 03:01:52 -0500

    update

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require('express'), 
-    static  = require('serve-static'),
-    favicon = require('serve-favicon'),
-    logger  = require('morgan'),
-    app     = express(), 
-    server  = require('http').createServer(app), 
-    path    = require('path'),
-    io      = require('socket.io').listen(server),
-    spawn   = require('child_process').spawn
+var express        = require('express'), 
+    static         = require('serve-static'),
+    favicon        = require('serve-favicon'),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override'),
+    logger         = require('morgan'),
+    app            = express(), 
+    server         = require('http').createServer(app), 
+    path           = require('path'),
+    io             = require('socket.io').listen(server),
+    spawn          = require('child_process').spawn
     
     
 // all environments
 app.set('port', process.env.TEST_PORT || 8080);
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(express.bodyParser());
+app.use(bodyParser());
 app.use(express.methodOverride());
 app.use(static(path.join(__dirname, 'public')));
 
