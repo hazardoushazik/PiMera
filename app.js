b655cedb9967484378c508c357a70c184ce47dfa
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-18 02:32:21 -0500

    update

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'), 
+    app    = express(), 
+    server = require('http').createServer(app), 
+    path   = require('path')
+    
+// all environments
+app.set('port', process.env.TEST_PORT || 8080);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html')
+});
+
+app.get('/remote', function (req, res) {
+    res.sendfile(__dirname + '/public/remote.html')
+});
+
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
