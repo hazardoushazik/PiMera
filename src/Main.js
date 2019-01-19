66fa60c03e6c781815e4878153b9c736b2cf2847
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-19 13:44:39 -0500

    update

--- src/Main.js
+++ src/Main.js
@@ -12,22 +12,33 @@ import Camera from "./Camera";
 import Data from "./Data";
 import Settings from "./Settings";
 
-const menuItems = [
-    {label: "Home", link: "/"},
+const items = [
+    {label: "Home",         link: "/"},
     {label: "Destinations", link: "/destinations"},
-    {label: "Media", link: "/media"},
-    {label: "Camera", link: "/camera"},
-    {label: "Data", link: "/data"},
-    {label: "Settings", link: "/settings"}
+    {label: "Media",        link: "/media"},
+    {label: "Camera",       link: "/camera"},
+    {label: "Data",         link: "/data"},
+    {label: "Settings",     link: "/settings"}
 ];
 
 
 function MainMenu (props) {
-    const menu = props.menuItems.map((menuItem) =>
-        <li><NavLink to = {menuItem.link}>{menuItem.label}</NavLink></li>
+    const menu = props.items.map((item) =>
+        <li><NavLink to = {item.link}>{item.label}</NavLink></li>
     );
+
     return (
         <ul>{menu}</ul>
+    );
+}
+
+function Content (props) {
+    const content = props.items.map((item) =>
+        <Route exact path = {item.link} component = { '${item.label}' }/>
+    );
+
+    return (
+        <div className = "content">{content}</div>
     );
 }
 
@@ -37,14 +48,15 @@ class Main extends Component {
             <HashRouter>
                 <div>
                     <span><h1>PiMera</h1></span>
-                    <MainMenu menuItems = {menuItems} />
+                    <MainMenu items = {items} />
+                    <Content  items = {items} />
                     <div className = "content">
-                        <Route exact path = "/" component = { Home }/>
+                        <Route exact path = "/"       component = { Home }/>
                         <Route path = "/destinations" component = { Destinations }/>
-                        <Route path = "/media" component = { Media }/>
-                        <Route path = "/camera" component = { Camera }/>
-                        <Route path = "/data" component = { Data }/>
-                        <Route path = "/settings" component = { Settings }/>
+                        <Route path = "/media"        component = { Media }/>
+                        <Route path = "/camera"       component = { Camera }/>
+                        <Route path = "/data"         component = { Data }/>
+                        <Route path = "/settings"     component = { Settings }/>
                     </div>
                 </div>
             </HashRouter>
