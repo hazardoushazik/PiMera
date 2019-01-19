4dac6a7a9b83426e8b36ce19c513337492310778
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-18 23:58:46 -0500

    update

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render () {
+        return (
+            <div>
+                <span><h1>Simple SPA</h1></span>
+                <ul className = "home">
+                    <li><a href = "">Home</a></li>
+                    <li><a href = "">Destinations</a></li>
+                    <li><a href = "">Media</a></li>
+                    <li><a href = "">Camera</a></li>
+                    <li><a href = "">OBD II Data</a></li>
+                    <li><a href = "">Settings</a></li>
+                </ul>
+                <div className = "content"></div>
+            </div>
+        );
+    }
+}
+
+export default Main;
