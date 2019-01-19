1b5fcd450c9879b08c25b2301a293b4ddfedadac
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-19 13:15:30 -0500

    update

--- src/Main.js
+++ src/Main.js
@@ -33,7 +33,7 @@ const menu_data = ({ LABELS, LINKS }) => (
     ]
 );
 
-const menu_items = menu_data.map((menu_data) => (
+const menu_items = menu_data.LABELS.map((menu_data) => (
     <li><NavLink to = {menu_data.LINKS}>{menu_data.LABELS}</NavLink></li>
 ));
 
