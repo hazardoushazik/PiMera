b19631ee93a8c08e05e57f0b245b3ed82ec5ec47
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-19 00:02:44 -0500

    update

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,12 @@ class Main extends Component {
             <div>
                 <span><h1>Simple SPA</h1></span>
                 <ul className = "home">
-                    <li><a href = "">Home</a></li>
-                    <li><a href = "">Destinations</a></li>
-                    <li><a href = "">Media</a></li>
-                    <li><a href = "">Camera</a></li>
-                    <li><a href = "">OBD II Data</a></li>
-                    <li><a href = "">Settings</a></li>
+                    <li><a href = "/">Home</a></li>
+                    <li><a href = "/destinations">Destinations</a></li>
+                    <li><a href = "/media">Media</a></li>
+                    <li><a href = "/camera">Camera</a></li>
+                    <li><a href = "/date">OBD II Data</a></li>
+                    <li><a href = "/settings">Settings</a></li>
                 </ul>
                 <div className = "content"></div>
             </div>
