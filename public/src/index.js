4cdaa4662f5362b092dedcb587306cde1a81bd89
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-18 22:56:13 -0500

    update

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Main from "./Main";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <Main />,
