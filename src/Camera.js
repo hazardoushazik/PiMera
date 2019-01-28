10694fdaadaf2147488ca4a7064dabf915a6467d
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 21:45:12 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
-import Main from "./Main";
-// Take still image and save to disk
-var socket = io();
-socket.on('liveStream', function(url) {
-	$('#stream').attr('src', url);
-	$('.start').hide();
-});
-
-function startStream() {
-	socket.emit('start-stream');
-	$('.start').hide();
-}
 
 class Camera extends Component {
 	render() {
 		return (
 			<div>
 				<h2>Camera</h2>
-				<div class="row" onload={startStream()}>
-					<img src="" id="stream"/>
+				<div>
+					<noscript>
+						<img src ="http://localhost:9000/?action=snapshot" 
+							 alt = "This is a static stream from my pi camera"/>
+					</noscript>
 				</div>
 			</div>
 		);
