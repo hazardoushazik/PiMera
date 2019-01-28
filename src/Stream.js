3f479730700cccd40f3f0eab2b8ba8e818ac5ffc
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-28 01:10:27 -0500

    update

--- src/Stream.js
+++ src/Stream.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 const STREAM_SRC = "http://raspberrypi:9000/?action=snapshot";
-const STREAM_REFRESH_RATE = 20;
+const STREAM_REFRESH_RATE = 40;
 
 class Stream extends Component {
 	constructor(props) {
