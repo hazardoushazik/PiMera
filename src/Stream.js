19fa73af1bfc83f66033ee9ba8a0c90c42302db3
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-28 00:28:16 -0500

    update

--- src/Stream.js
+++ src/Stream.js
@@ -17,7 +17,7 @@ class Stream extends Component {
                 canvas.height = this.height;
         
                 var ctx = canvas.getContext("2d");
-                ctx.drawImage(this, 0, 0);
+                // ctx.drawImage(this, 0, 0);
         
                 var dataURL = canvas.toDataURL("image/png");
                 component.setState({liveImage: dataURL});
