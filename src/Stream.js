1c7e00d263fbd4c860f844fd6f61fcba6eaeb51c
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-28 00:10:59 -0500

    update

--- src/Stream.js
+++ src/Stream.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
 
 const STREAM_SRC = "http://raspberrypi:9000/?action=snapshot";
+const STREAM_REFRESH_RATE = 20;
 
 class Stream extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			currentImage: null,
-			nextImage: `${STREAM_SRC}?${new Date().getTime()}`
-		};
-	}
-
-	componentDidMount() {
-		this.interval = setInterval(
-			() => this.setState({
-                currentImage: this.nextImage,
-				nextImage: `${STREAM_SRC}?${new Date().getTime()}`,
-			}),
-			20
-		);
-	}
+        this.loadImage = () => {
+            const component = this;
+      
+            const img = new Image();
+            img.crossOrigin = "Anonymous";
+            img.onload = function () {
+                var canvas = document.createElement("canvas");
+                canvas.width =this.width;
+                canvas.height =this.height;
+        
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(this, 0, 0);
+        
+                var dataURL = canvas.toDataURL("image/png");
+                component.setState({liveImage: STREAM_SRC});
+            };
+        
+                img.src = `${this.props.image}?${new Date().getTime()}`;
+                this.setState({ loadingImage: img });
+        }
+      
+        this.state = {
+            loadingImage: null,
+            liveImage: null
+        };
+    }
+      
+    componentDidMount() {
+        this.loadImage();
+        this.interval = setInterval(this.loadImage, STREAM_REFRESH_RATE);
+    }
 	
 	componentWillUnmount(nextProps) {
 		clearInterval(this.interval);
@@ -27,7 +44,7 @@ class Stream extends Component {
 	
 	render() {
 		return (
-            <img 	src = {this.state.currentImage}
+            <img 	src = {this.state.streamImage}
 					alt = "This is a static stream from my pi camera"/>
 		)
 	}
