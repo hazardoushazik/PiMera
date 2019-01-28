fef23059737164377a4d7e9e277bcffc036832cc
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 21:40:16 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 // Take still image and save to disk
+var socket = io();
+socket.on('liveStream', function(url) {
+	$('#stream').attr('src', url);
+	$('.start').hide();
+});
+
+function startStream() {
+	socket.emit('start-stream');
+	$('.start').hide();
+}
 
 class Camera extends Component {
 	render() {
-		var socket = io();
-		socket.on('liveStream', function(url) {
-			$('#stream').attr('src', url);
-			$('.start').hide();
-		});
-		
-		function startStream() {
-			socket.emit('start-stream');
-			$('.start').hide();
-		}
 		return (
 			<div>
 				<h2>Camera</h2>
