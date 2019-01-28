64af43a07c8de40c42aecf20a7e9b3235a144eb2
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 22:32:17 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -6,10 +6,10 @@ class Camera extends Component {
 			<div>
 				<h2>Camera</h2>
 				<div>
-					{/* <noscript> */}
-						<img src ="http://raspberrypi:9000/?action=snapshot"
-							 alt = "This is a stream from my pi camera"/>
-					{/* </noscript> */}
+					<noscript>
+						<img src ="http://localhost:9000/?action=snapshot" 
+							 alt = "This is a static stream from my pi camera"/>
+					</noscript>
 				</div>
 			</div>
 		);
