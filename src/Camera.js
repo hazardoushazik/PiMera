531eca6e9eaebaf5723006336c725c0f2b677d42
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 22:29:02 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 
-const image_url = "http://raspberrypi:9000/?action=snapshot";
-
-function tick() {
-	const image_url = "http://raspberrypi:9000/?action=snapshot";
-}
-
 function Image() {
 	return (
-		<img src = {image_url}
+		<img src = "http://raspberrypi:9000/?action=snapshot"
 			 alt = "This is a stream from my pi camera"/>
 	)
 }
-setInterval(tick, 16); // approximate 30 FPS (1000 msec / 1 sec) * (1 sec / 60 frames)
+// setInterval(tick, 16); // approximate 30 FPS (1000 msec / 1 sec) * (1 sec / 60 frames)
 
 class Camera extends Component {
 	render() {
