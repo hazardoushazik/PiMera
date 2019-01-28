506ceaffcea923d725d05868ae9f983d3160fbfd
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 23:50:04 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -2,14 +2,6 @@ import React, { Component } from "react";
 
 const STREAM_SRC = "http://raspberrypi:9000/?action=snapshot";
 
-function Stream(props) {
-	const hash = new Date().toLocaleTimeString();
-	return (
-		<img 	src = {`${props.src}?${hash}`}
-				alt = "This is a static stream from my pi camera"/>
-	)
-}
-
 class Camera extends Component {
 	constructor(props) {
 		super(props);
