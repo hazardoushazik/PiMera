8c3e261eff4c05299f618eeccaf22e3b7925290f
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 22:25:42 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -1,44 +1,26 @@
 import React, { Component } from "react";
 
-var imageNr = 0; // Serial number of current image
-var finished = new Array(); // References to img objects which have finished downloading
-var paused = false;
 
-function createImageLayer() {
-	var img = new Image();
-	img.style.position = "absolute";
-	img.style.zIndex = -1;
-	img.onload = imageOnload;
-	img.onclick = imageOnclick;
-	img.src = "./?action=snapshot&n=" + (++imageNr);
-	var webcam = document.getElementById("webcam");
-	webcam.insertBefore(img, webcam.firstChild);
+function tick() {
+	const image_url = "http://raspberrypi:9000/?action=snapshot";
 }
 
-// Two layers are always present (except at the very beginning), to avoid flicker
-function imageOnload() {
-	this.style.zIndex = imageNr; // Image finished, bring to front!
-	while (1 < finished.length) {
-		var del = finished.shift(); // Delete old image(s) from document
-		del.parentNode.removeChild(del);
-	}
-	finished.push(this);
-	if (!paused) createImageLayer();
+function Image() {
+	return (
+		<img src = {image_url}
+			 alt = "This is a stream from my pi camera"/>
+	)
 }
-function imageOnclick() { // Clicking on the image will pause the stream
-	paused = !paused;
-	if (!paused) createImageLayer();
-}
+setInterval(tick, 16); // approximate 30 FPS (1000 msec / 1 sec) * (1 sec / 60 frames)
 
 class Camera extends Component {
 	render() {
 		return (
-			<div onLoad={createImageLayer()}>
+			<div>
 				<h2>Camera</h2>
-				<div id="webcam">
+				<div>
 					<noscript>
-						<img src ="http://raspberrypi:9000/?action=snapshot"
-							 alt = "This is a stream from my pi camera"/>
+						<Image />
 					</noscript>
 				</div>
 			</div>
