46e68932f51b879b369181436c94ada577c5fc72
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 21:28:06 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import spawn from "child_process";
 import { StillCamera, Codec } from "pi-camera-connect";
 import * as fs from "fs";
 // Take still image and save to disk
