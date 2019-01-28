0a5e184a41a4e9016d73585b753e6cd978df55d4
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 23:53:21 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -1,35 +1,13 @@
 import React, { Component } from "react";
+import Stream from "./Stream";
 
-const STREAM_SRC = "http://raspberrypi:9000/?action=snapshot";
-
-class Camera extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			streanImage: null
-		};
-	}
-
-	componentDidMount() {
-		this.interval = setInterval(
-			() => this.setState({
-				streanImage: `${STREAM_SRC}?${new Date().getTime()}`,
-			}),
-			10
-		);
-	}
-	
-	componentWillUnmount(nextProps) {
-		clearInterval(this.interval);
-	}
-	
+class Camera extends Component {	
 	render() {
 		return (
 			<div>
 				<h2>Camera</h2>
 				<div>
-					<img 	src = {this.state.streanImage}
-							alt = "This is a static stream from my pi camera"/>
+					<Stream /> 
 				</div>
 			</div>
 		)
---
+++ src/Stream.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+const STREAM_SRC = "http://raspberrypi:9000/?action=snapshot";
+
+class Stream extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			streamImage: null
+		};
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(
+			() => this.setState({
+				streanImage: `${STREAM_SRC}?${new Date().getTime()}`,
+			}),
+			10
+		);
+	}
+	
+	componentWillUnmount(nextProps) {
+		clearInterval(this.interval);
+	}
+	
+	render() {
+		return (
+            <img 	src = {this.state.streamImage}
+					alt = "This is a static stream from my pi camera"/>
+		)
+	}
+}
+export default Camera;
