5a2508e959a063cf5c0aefa2f90aa97e98a6af52
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-27 21:20:36 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 import { StreamCamera, Codec } from "pi-camera-connect";
 import * as fs from "fs";
-
-// Take still image and save to disk
-const streamCamera = new StreamCamera({
-	codec: Codec.MJPEG
-});
-
-streamCamera.startCapture().then(
-	streamCamera.takeImage().then(image => {
-		fs.writeFileSync("stream-image.jpg", image);
-	}),
-	streamCamera.stopCapture()
-);
-
 class Camera extends Component {
 	render() {
+
+		// Take still image and save to disk
+		const streamCamera = new StreamCamera({
+			codec: Codec.MJPEG
+		});
+		
+		streamCamera.startCapture().then(
+			streamCamera.takeImage().then(image => {
+				fs.writeFileSync("stream-image.jpg", image);
+			}),
+			streamCamera.stopCapture()
+		);
+		
 		return (
 			<div>
 				<h2>Camera</h2>
