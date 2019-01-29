72315c0e787930f5cffdda4ac4a197bdcc03177b
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-28 23:02:17 -0500

    update

--- src/Stream.js
+++ src/Stream.js
@@ -21,7 +21,7 @@ class Stream extends Component {
 	
 	streamStyle = {
 		transform: 'rotate(180deg)',
-		backgroundImage: 'url(' + this.state.streamImage + ')',
+		backgroundImage: 'url(' + `${this.state.streamImage}` + ')',
 	};
 	
 	componentWillUnmount(nextProps) {
