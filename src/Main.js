236ccd477c55e12299302b176cb2c8aa7306674e
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-29 17:42:27 -0500

    update

--- src/Main.js
+++ src/Main.js
@@ -49,7 +49,11 @@ const items = [
 
 function MainMenu (props) {
     const menu = props.items.map((item) =>
-        <NavLink className = "btn nav-link" key = {item.label} to = {item.link}>{item.label}</NavLink>
+        <NavLink    className = "btn btn-primary nav-link" 
+                    key = {item.label} 
+                    to = {item.link}>
+            {item.label}
+        </NavLink>
     );
 
     return (
