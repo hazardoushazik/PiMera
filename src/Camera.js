42ba625032c5f80d6b1aa097f2e56b15b10589c7
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-29 17:15:24 -0500

    update

--- src/Camera.js
+++ src/Camera.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import Stream from "./Stream";
 
 class Camera extends Component {	
+	constructor(props) {
+		super(props);
+		this.myRef = React.createRef();		
+	}
 	render() {
 		return (
-			<div>
+			<div ref = {this.myRef}>
 				<h2>Camera</h2>
 				<Stream /> 
 			</div>
 		)
 	}
+	scrollToMyRef = () => window.scrollTo(0, this.myRef.current.offsetTop);
 }
 export default Camera;
