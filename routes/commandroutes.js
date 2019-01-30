e004e403f2926d385996db1718e09c504b124c3e
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-30 01:52:02 -0500

    update

---
+++ routes/commandroutes.js
@@ -0,0 +1,13 @@
+let cmd=require('node-cmd');
+exports.commandrun = function(req,res){
+    console.log("command req",req.body.command);
+    cmd.get(
+        req.body.command,
+        function(data){            
+            res.send({
+                "code":"200",
+                "result":data
+            })
+        }
+    );
+}
--- src/Settings.js
+++ src/Settings.js
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
- 
+import Terminal from 'react-bash';
+
+const history = [
+    { value: 'Type `help` to begin' },
+];
+
+
 class Settings extends Component {
-  render() {
-    return (
-      <div>
-        <p>Some site/pi settings</p>
-        <ol>
-          <li>Setting 1</li>
-          <li>Setting 2</li>
-          <li>Setting 3</li>
-          <li>Setting 4</li>
-        </ol>
-      </div>
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state={
+			commandData:''
+		}
+	}
+	render() {
+		const extensions = {
+			sudo: {
+				exec: ({structure, history, cwd}) => {
+					return {structure, cwd,
+						history: history.concat({value: 'Nice try...'}),
+					};
+				},
+			},
+		}
+		return (
+			<div className="parentContainer">
+				<h4>Run docker administration commands here</h4>
+				<div style={{flex:1}}>
+                  	<Terminal history={history} structure={structure} extensions={extensions} prefix={"pi@raspberrypi"}/>  
+				</div>
+				<div>
+					{this.state.commandData}
+				</div> 
+			</div>
+		);
+	}
 }
 
 export default Settings;
